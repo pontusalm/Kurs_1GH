55ce255587277db6e1089a8d9d8baa9ce0026592
pontusalm <pontus.alm@gmail.com>
2021-10-02 16:22:27 +0200

    v2.loops

---
+++ v2_Loopar.py
@@ -0,0 +1,46 @@
+# # Loop#1
+# for tal in range(0,11):
+#     print (tal)
+
+# tal=0
+# while tal<11:
+#     print (tal)
+#     tal=tal+1
+
+# Loop#2
+# tal1=int(input("Skriv in tal 1: "))
+# tal2=int(input("Skriv in tal2: "))
+# # for tal in range(tal1+1,tal2):
+# #     print (tal)
+
+# tal=tal1+1
+# while tal<tal2:
+#     print (tal)
+#     tal=tal+1
+
+# Loop#3
+# while True:
+#     tal1=int(input("Ange tal1: "))
+#     tal2=int(input("Ange tal2: "))
+#     summa=tal1+tal2
+#     print(f"summan är {summa}")
+#     sel=input("Vill du fortsätta? J/N ")
+#     if sel==("N"):
+#         print("Vi slutar")
+#         break
+#     else:
+#         continue
+
+# Loop#4
+# iteration=0
+# talSumma=0
+# while iteration<10:
+#     tal=int(input("Mata in ett tal: "))
+#     talSumma=talSumma+tal
+#     iteration=iteration+1
+# print (f"Summan av dina tal är {talSumma}")
+
+# Loop#5
+
+while True:
+    tal=
